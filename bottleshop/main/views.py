f0bd164e4a0cb1c47d1532904efcdffd0a106bba
Tooni <prashanth04@gmail.com>
2022-10-11 22:07:58 +0530

    Frontend Commit - 1 Templates and Static and Media Files

--- bottleshop/main/views.py
+++ bottleshop/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import (
+    Banner,
+    Category,
+    Brand,
+    ProductAttribute,
+    Product,
+    Color,
+    Size,
+)
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html")
